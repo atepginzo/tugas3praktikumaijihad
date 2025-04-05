2468adcc1a5e22ad342497db1bfbacb9e25bfb67
jihadakbar911 <jihadar165@gmail.com>
2025-04-05 21:27:49 +0700

    (uploadulanng)

--- venv/app.py
+++ venv/app.py
@@ -52,5 +52,5 @@ def predict_manual():
 # Jalankan Aplikasi
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
     
